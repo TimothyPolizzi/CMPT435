e8cc7928c113248d5f4b1d0841098e5008f7c594
Tim <timpolizzi2@gmail.com>
2019-04-17 18:07:05 -0400

    Started Linked Object Graph
    Work has been done, I'm done eating so time to head back to the cave. Finished up the logical bits but those are probably gonna get reworked when I finish/start the traversals. Time for a good time.

---
+++ Assignments/AssignmentThree/LinkedGraph.py
@@ -0,0 +1,87 @@
+# A graph made of linked objects for Alan's CMPT435.
+
+__author__ = 'Tim Polizzi'
+__email__ = 'Timothy.Polizzi1@marist.edu'
+
+
+class LinkedGraph(object):
+
+    class __Node(object):
+        """ An internal Node for the Linked Object model of the Graph
+
+        """
+        def __init__(self, *init_value):
+            if 2 > len(init_value) > 0:
+                self.value = init_value
+            else:
+                self.value = None
+            self.connected_nodes = []
+
+        def connect(self, node):
+            self.connected_nodes.append(node)
+
+        def set_value(self, value: int):
+            self.value = value
+
+    def __init__(self):
+        self.vertex_list = []
+
+    def add_vertex(self, to_add: int):
+        """ Adds a vertex to the graph.
+
+        Args:
+            to_add: The value of the vertex to add.
+
+        Returns:
+            A boolean value that returns True if it worked, False otherwise.
+        """
+        to_return = True
+
+        self.vertex_list.append(self.__Node(to_add))
+
+        return to_return
+
+    def find_node(self, to_find: int) -> __Node:
+        """ Finds a node in the graph based on it's value.
+
+        Args:
+            to_find: The integer value of the node that is being searched for.
+
+        Returns:
+            The node who's value corresponds to the integer value queried, or None if the node could not be found.
+        """
+        to_return = None
+
+        for node in self.vertex_list:
+            if node.value is to_find:
+                to_return = node
+
+        return to_return
+
+    def add_edge(self, vertex_1: int, vertex_2: int):
+        """ Adds an edge between two vertices to the graph.
+
+        Args:
+            vertex_1: The first vertex to be connected.
+            vertex_2: The second vertex to be connected.
+
+        Returns:
+            A boolean value that is True if the edge is created, False otherwise.
+        """
+        to_return = True
+
+        temp1 = self.find_node(vertex_1)
+        temp2 = self.find_node(vertex_2)
+
+        temp1.connected_nodes.append(temp2)
+        temp2.connected_nodes.append(temp1)
+
+        return to_return
+
+    def breadth_first_traversal(self):
+        # TODO: Implement
+        return
+
+    def depth_first_traversal(self):
+        # TODO: Implement
+        return
